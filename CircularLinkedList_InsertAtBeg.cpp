4129ec7b9c807d3af9d4889364d618c8cce82399
kira <chitimellapraneeth@gmail.com>
2023-02-02 19:24:31 +0530

    Added second cpp program, circular linked list

---
+++ CircularLinkedList_InsertAtBeg.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+return 0;
+}#include<iostream>
+using namespace std;
+struct Node{
+	int data;
+	Node *next;
+	Node(int x){
+		data=x;
+		next=NULL;
+	}
+};
+Node *insertEnd(Node *head,int x){
+	Node *temp=new Node(x);
+	if(head==NULL){
+		temp->next=temp;
+		return temp;
+	}
+	else{
+		Node *curr=head;
+		while(curr->next!=head)
+		curr=curr->next;
+		curr->next=temp;
+		temp->next=head;
+		return head;
+	}
+}
+int main()
+{
+	Node *head=NULL;
+	head=insertAtEnd(head,10);
+	head=insertAtEnd(head,20);
+	return 0;
+}
+
