26b72b46951eadb242d1f5becbe038ed544d1669
kira <chitimellapraneeth@gmail.com>
2023-02-08 23:40:19 +0530

    added new file maximum rectangle

---
+++ maximumRectangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+return 0;
+}int maxRectangle(int mat[r][c]){
+	int res=largesthist(mat[0],c);//we are passing only first row to find the largest histogram function from prev question
+
+	for(int i=1;i<r;i++){//begin with second row
+		for(int j=0;j<;j++){//we consider the curr row as a base for 
+			if(mat[i][j]==1) mat[i][j] +=mat[i-1][j];//we add the above value of curr row 
+		}
+		res=max(res,largesthist(mat[i],c));
+	}
+	return res;
+}
